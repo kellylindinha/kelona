7e3a4c8801d9565e877b54a37ca6626fba3393f6
kellylindinha <kelly.valente@escola.pr.gov.br>
2026-09-02 11:31:11 -0300

    ççlklollol

---
+++ script.js
@@ -0,0 +1,6 @@
+<script src="script.js"></script>
+<script src="script.js"></script>
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
