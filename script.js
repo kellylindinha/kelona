500b24f582f7485afe5db3f2f8b078407cf95439
kellylindinha <kelly.valente@escola.pr.gov.br>
2026-09-02 11:32:03 -0300

    hfcf

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ const textoResultado = document.querySelector(".texto-resultado");
 const pergunta1 = "Pergunta 1";
 const pergunta2 = "Pergunta 2";
 const perguntas = ["Pergunta 1", "Pergunta 2"];
+const perguntas = [
+  {
+    enunciado: "Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+  {
+    enunciado: "Pergunta 2",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+];
